e4a8234db5710923ca80e9fbaedcac774216409e
Elijas <master.elijas@gmail.com>
2014-12-10 21:11:59 +0200

    Fully finished layer size independent forward and backward propagations

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -1,9 +1,10 @@
 function [J grad] = nnCostFunction(nn_params, nn_lsizes, X, y, lambda)
     y = eye(nn_lsizes(end))(y,:);
 
-    a = X; %a contains horizontal vectors of neuron activation values;
-    Jvec_params = []; %Useful for later regularization of Cost Function
-    gradvec_params = []; %Useful for later to regularize gradients
+    a = X; %ontains horizontal vectors of neuron activation values;
+    indices_a = []; %contains start-end indices for neurons of different layers (to group neurons by layers)
+    Jvec_params = []; %useful to regularize Cost Function
+    gradvec_params = []; %useful to regularize gradients
     a_pos = 0;
     params_pos = 0;
     for i=1:size(nn_lsizes,2)-1;
@@ -13,6 +14,8 @@ function [J grad] = nnCostFunction(nn_params, nn_lsizes, X, y, lambda)
 
         a = [a  sigmoid([ones(size(a,1),1) a(:,a_pos+1:a_pos+nn_lsizes(i))] * Theta')];
 
+        indices_a = [indices_a [a_pos+1;a_pos+nn_lsizes(i)]];
+
         Jvec_params = [Jvec_params; Theta(:,2:end)(:)];
         Theta(:,1)=0;
         gradvec_params = [gradvec_params; Theta(:)];
@@ -20,6 +23,7 @@ function [J grad] = nnCostFunction(nn_params, nn_lsizes, X, y, lambda)
         params_pos += ThetaRows*ThetaCols;
         a_pos += nn_lsizes(i);
     end
+    indices_a = [indices_a [a_pos+1;a_pos+nn_lsizes(end)]];
 
     % COST
     Jvec_h = a(:,end-nn_lsizes(end)+1:end)(:);
@@ -27,21 +31,38 @@ function [J grad] = nnCostFunction(nn_params, nn_lsizes, X, y, lambda)
     J = -1/size(a,1) .* (Jvec_y'*log(Jvec_h) + (1-Jvec_y)'*log(1-Jvec_h));
     J += lambda/(2*size(a,1)) * sum(sum(Jvec_params.*Jvec_params));
 
-    d = a(:,end-nn_lsizes(end)+1:end) - y;
-LGG=true;
-    for i=fliplr(2:size(nn_lsizes,2));
+    % GRADIENTS
+    grad = (  1/size(a,1)  *  (a(:,end-nn_lsizes(end)+1:end) - y)'  *  [ones(size(a,1),1) a(:,indices_a(1,size(nn_lsizes,2)-1):indices_a(2,size(nn_lsizes,2)-1))]  )(:);
+    delta = a(:,end-nn_lsizes(end)+1:end) - y;
+    for i=fliplr(3:size(nn_lsizes,2))
         ThetaRows = nn_lsizes(i);
         ThetaCols = nn_lsizes(i-1)+1;
-
         params_pos -= ThetaRows*ThetaCols;
-        a_pos -= nn_lsizes(i)
-        a_posUL =  a_pos+nn_lsizes(i)
-
         Theta = reshape(nn_params(params_pos+1 : params_pos+ThetaRows*ThetaCols), ThetaRows, ThetaCols);
-
-        if LGG,  a, d*Theta(:,2:end).*sigmoidGrad(a(:,a_pos:a_pos+nn_lsizes(i)))    ,LGG=false;end
-
-        
+        delta = delta * Theta(:,2:end) .* sigmoidGrad(a(:,indices_a(1,i-1):indices_a(2,i-1)));
+        grad = [( 1/size(a,1) * delta' * [ones(size(a,1),1) a(:,indices_a(1,i-2):indices_a(2,i-2))] )(:); grad];       
     end
+    grad += lambda/size(a,1) .* gradvec_params;
+
 endfunction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
